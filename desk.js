d923584ceb630139324ac6892a1a8394f53cd9c8
SudiptaSundarPramanik <79316024+SudiptaSundarPramanik@users.noreply.github.com>
2021-12-06 19:48:56 +0530

    Add files via upload

--- desk.js
+++ desk.js
@@ -1,5 +1,7 @@
 img="";
 status="";
+objects=[];
+objectDetector="";
 
 function preload(){
     img=loadImage("desk.jpg");
@@ -9,13 +11,13 @@ function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("desk_detect").innerHTML="Status : Detecting objects👀";
+    document.getElementById("desk_status").innerHTML="Status : Detecting objects👀";
 }
 
 function modelLoaded(){
     console.log("Model is Loaded?");
     status=true;
-    objectDetector.detect(video,gotResult);
+    objectDetector.detect(img,gotResult);
 }
 
 function gotResult(error,results){
@@ -23,8 +25,26 @@ function gotResult(error,results){
         console.error(error);
     }
     console.log(results);
+    objects=results;
 }
 
 function draw(){
     image(img,0,0,640,420);
+
+    if(status!=" "){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        objectDetector.detect(img,gotResult);
+        for(i=0;i<objects.length;i++){
+            document.getElementById("desk_status").innerHTML="Status: Detecting Objects⚡⚡";
+            document.getElementById("desk_objects").innerHTML="Number of objects are⚡⚡ : "+objects.length;
+            fill(r,g,b);
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+" % ",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
